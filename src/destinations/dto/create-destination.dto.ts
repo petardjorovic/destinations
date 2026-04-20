f00e375337159ce1abd1d477bcb1c09e198aec63
petardjorovic <perodjorovic@gmail.com>
2026-04-20 20:53:16 +0200

    swagger integration

--- src/destinations/dto/create-destination.dto.ts
+++ src/destinations/dto/create-destination.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsDateString,
@@ -10,19 +11,39 @@ import {
 } from 'class-validator';
 
 export class CreateDestinationDto {
+  @ApiProperty({
+    required: true,
+    name: 'name',
+    example: 'Barcelona trip',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   name!: string;
 
+  @ApiProperty({
+    required: false,
+    name: 'travelDate',
+    example: '2026-02-22',
+  })
   @IsOptional()
   @IsDateString()
   travelDate?: string;
 
+  @ApiProperty({
+    required: false,
+    name: 'notes',
+    example: 'Some note example',
+  })
   @IsString()
   @IsOptional()
   notes?: string;
 
+  @ApiProperty({
+    required: true,
+    name: 'userId',
+    example: '123',
+  })
   @Type(() => Number)
   @IsInt()
   @Min(1)
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,19 @@ async function bootstrap() {
       transform: true, // now data will be instance of DTO class
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Destinations')
+    .setDescription('The destinations API description')
+    .setVersion('1.0')
+    .addTag('destinations')
+    .addTag('tweets')
+    .addTag('users')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
